2aafcd8d085e8e1fa87e465751d97c82dcf471d3
Raghdaa <raghdaae@std.mans.edu.eg>
2026-09-02 23:51:48 +0300

    refactor: remove redundant root backend folder

--- backend/middlewares/error_handler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (error, req, res, next) => {
-    res.status(error.status || 500).json({
-        msg: "Error middleware",
-        error_name: error.name,
-        error_message: error.message
-    });
-};
